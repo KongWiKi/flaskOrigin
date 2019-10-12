275a7dc79a7034533c3ae3eaf8136f50adacb84b
KongWiki <kongwiki@163.com>
2019-10-12 14:26:33 +0800

    一些验证

--- forKnow/flash.py
+++ forKnow/flash.py
@@ -1,6 +1,13 @@
 """
 @time : 2019/9/24下午9:30
 @Author: kongwiki
-@File: flush.py
+@File: flash.py
 @Email: kongwiki@163.com
 """
+session = dict()
+
+# session['_flash'] = ['hello, this is flash object values']
+flashes = session.get('_flash', [])
+flashes.append('append new message')
+session['_flash'] = flashes
+print(session['_flash'])
--- forKnow/wsgiKnow.py
+++ forKnow/wsgiKnow.py
@@ -4,3 +4,27 @@
 @File: wsgiKnow.py
 @Email: kongwiki@163.com
 """
+from wsgiref.simple_server import make_server
+
+
+def hello(environ, start_response):
+	status = '200 ok'
+	response_headers = [('Content-type', 'text/html')]
+	name = environ['PATH_INFO'][1:]
+	start_response(status, response_headers)
+	return [b'<h1>Hello, Web</h1>' ]
+
+
+# class AppClass:
+# 	def __init__(self, environ, start_response):
+# 		self.environ = environ
+# 		self.start_response = start_response
+#
+# 	def __iter__(self):
+# 		status = '200'
+# 		response_headers = [('Content-type', 'text/html')]
+# 		self.start_response(status, response_headers)
+# 		yield b'<h1>Hello Web!</h1>'
+
+server = make_server('localhost', 5000, hello)
+server.serve_forever()
